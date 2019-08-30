d7f7e93bcaaf275bfeff3d08d911d9248a9d47be
Seham1981 <seham.hammad@yahoo.ie>
2019-08-30 21:13:05 +0100

    new updates

---
+++ src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+class Landing extends React.Component {
+  
+    render() { 
+        return (  
+            <div className="container">
+                <div className="col-sm-8 mx-auto">
+                    <h2 className="text-center"> Welcome to our Login System</h2>
+                </div>
+            </div>
+        );
+    }
+}
+ 
+export default Landing;
